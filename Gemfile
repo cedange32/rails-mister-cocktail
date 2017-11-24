abe5bd6c110e29fcea050d2a654f2008c41e6e03
charles edange <charlesedange@hotmail.com>
2017-11-24 19:11:31 +0100

    update carrierwave gem

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'autoprefixer-rails'
 
 gem 'figaro'
 gem 'cloudinary'
-gem 'carrierwave'
+gem 'carrierwave', '~> 1.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,14 +2,17 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
   def show
     @cocktail = Cocktail.find(params[:id])
     @doses = @cocktail.doses
     @dose = Dose.new
   end
+
   def new
     @cocktail = Cocktail.new
   end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
